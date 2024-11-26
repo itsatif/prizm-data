1d9959ccbb08f1fcebdc22c23599f2e04e565371
atif <atif8486@gmail.com>
2024-11-26 14:21:51 +0530

    home page feature update

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,119 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterLink } from '@angular/router';
+import { NavigationEnd, Router, RouterLink } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule, RouterLink, MatButtonModule, MatIconModule],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.css',
 })
-export class NavBarComponent {
-  constructor() {}
+export class NavBarComponent implements OnInit {
+  toggleSideNav: boolean = false;
+  toggleServices: boolean = false;
+  toggleResources: boolean = false;
 
-  toggleMenu() {
-    this.closeAllSubMenu();
-    const navigationMenu = document.querySelector(
-      '.navigation-menu',
-    ) as HTMLElement;
-    const hamburger = document.querySelector('.hamburger') as HTMLElement;
-    const body = document.querySelector('body') as HTMLElement;
+  constructor(private router: Router) {}
 
-    if (navigationMenu.classList.contains('active')) {
-      hamburger.classList.toggle('open');
-      body.classList.toggle('overflow');
-      navigationMenu.classList.remove('active');
-      setTimeout(() => {
-        document
-          .querySelectorAll(
-            '.navigation-menu .inner .menu, .navigation-menu .inner blockquote',
-          )
-          .forEach((element) => {
-            (element as HTMLElement).style.transitionDelay = '0s';
-          });
-        document
-          .querySelectorAll('.navigation-menu .bg-layers span')
-          .forEach((element) => {
-            (element as HTMLElement).style.transitionDelay = '0.3s';
-          });
-      }, 0);
-    } else {
-      navigationMenu.classList.add('active');
-      hamburger.classList.toggle('open');
-      body.classList.toggle('overflow');
-      setTimeout(() => {
-        document
-          .querySelectorAll('.navigation-menu.active .inner .menu')
-          .forEach((element) => {
-            (element as HTMLElement).style.transitionDelay = '0.45s';
-          });
-        document
-          .querySelectorAll('.navigation-menu.active .inner blockquote')
-          .forEach((element) => {
-            (element as HTMLElement).style.transitionDelay = '0.50s';
-          });
-      }, 0);
-    }
-  }
-
-  onPageLinkClick(event: Event) {
-    const target = event.target as HTMLAnchorElement;
-    const url = target.href;
-    if (url && url.indexOf('#') !== -1) {
-      const hash = url.substring(url.indexOf('#'));
-      const targetElement = document.querySelector('body ' + hash);
-      if (targetElement) {
-        const navigationMenu = document.querySelector(
-          '.navigation-menu',
-        ) as HTMLElement;
-        const hamburger = document.querySelector('.hamburger') as HTMLElement;
-        const body = document.querySelector('body') as HTMLElement;
-
-        document.querySelectorAll('.transition-overlay').forEach((element) => {
-          element.classList.remove('active');
-        });
-        hamburger.classList.toggle('open');
-        body.classList.toggle('overflow');
-        navigationMenu.classList.remove('active');
-        setTimeout(() => {
-          document
-            .querySelectorAll('.navigation-menu .inner ul')
-            .forEach((element) => {
-              (element as HTMLElement).style.transitionDelay = '0s';
-            });
-          document
-            .querySelectorAll('.navigation-menu .inner blockquote')
-            .forEach((element) => {
-              (element as HTMLElement).style.transitionDelay = '0s';
-            });
-          document
-            .querySelectorAll('.navigation-menu .bg-layers span')
-            .forEach((element) => {
-              (element as HTMLElement).style.transitionDelay = '0.3s';
-            });
-          window.scrollTo({
-            top: (targetElement as HTMLElement).offsetTop,
-            behavior: 'smooth',
-          });
-        }, 0);
-      }
-    }
-  }
-
-  openSubmenu(event: any): void {
-    this.closeAllSubMenu();
-    const { target } = event;
-    target.lastChild.classList.add('open');
-  }
-
-  closeAllSubMenu(): void {
-    const allSubMenu = document.querySelectorAll(
-      '.navigation-menu .inner .menu ul li ul',
-    );
-    allSubMenu.forEach((subMenu: Element): void => {
-      if (subMenu.classList.contains('open')) {
-        subMenu.classList.remove('open');
+  ngOnInit(): void {
+    this.router.events.subscribe((event): void => {
+      if (event instanceof NavigationEnd) {
+        this.toggleSideNav = false;
+        this.toggleServices = false;
+        this.toggleResources = false;
       }
     });
   }
