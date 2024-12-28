f5418f068ae174380597ace358dbbe37a0b5bc58
atif <atif8486@gmail.com>
2024-12-28 06:35:30 +0530

    Angular i18n international language translation

--- src/app/upcoming-events/upcoming-events.component.ts
+++ src/app/upcoming-events/upcoming-events.component.ts
@@ -1,10 +1,11 @@
 import {Component, OnInit} from '@angular/core';
 import {CommonModule} from "@angular/common";
+import {TranslatePipe} from "@ngx-translate/core";
 
 @Component({
   selector: 'app-upcoming-events',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TranslatePipe],
   templateUrl: './upcoming-events.component.html',
   styleUrl: './upcoming-events.component.css'
 })
